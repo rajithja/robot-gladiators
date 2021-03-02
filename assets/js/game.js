2698719ef15b9d880ccd0d72e12294bff253b74d
rajithjay <rajithjayakuru@gmail.com>
2021-03-02 00:30:46 -0500

    after debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,8 +14,11 @@ console.log(enemyNames [2]);
 console.log(enemyNames.length);
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
+    // place fight function code block here . . .
+  
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   
@@ -63,8 +66,11 @@ var fight = function(enemyName) {
       fight();
     }
     }
+  }
   };
 
   for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }
